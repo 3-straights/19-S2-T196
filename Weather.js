783d5487770fa1ecb353a11f78077ae8aae2a1fb
Tang Yi Zhe <ytan0133@student.monash.edu>
2019-10-14 18:40:40 +0800

    Weather done, but need to amend

--- Weather.js
+++ Weather.js
@@ -1,18 +1,22 @@
+//Weather of initial port
 function intPortWeather()
 {
-  let data = {
-    latitude = JSON.parse(localStorage.getItem('intPort')).lat;
-    longtitude = JSON.parse(localStorage.getItem('intPort')).lng;
-  };
-    //"https://api.darksky.net/forecast/f77205dbd3303a32b0147c56de128ed0/[latitude],[longitude]"
+  document.getElementById("intPortName").innerHTML = JSON.parse(localStorage.getItem('intPort')).portName;
+  let data =
+    {
+      latitude: JSON.parse(localStorage.getItem('intPort')).lat,
+      longitude: JSON.parse(localStorage.getItem('intPort')).lng
+    };
+  //apikey: 2f325944bde3b00d86bdabaf4ac091be
+  //"https://api.darksky.net/forecast/2f325944bde3b00d86bdabaf4ac091be/[latitude],[longitude]"
   let data_needed = {
     exclude:["minutely","hourly","daily","flags","offset"],
     units:"si"
   };
-  let link_needed = "https://api.darksky.net/forecast/f77205dbd3303a32b0147c56de128ed0/" + data.latitude + "," + data.longitude + "?exclude=minutely&exclude=hourly&exclude=daily&exclude=flags&exclude=offset&units=si&callback=test"
+  let link_needed = "https://api.darksky.net/forecast/2f325944bde3b00d86bdabaf4ac091be/" + data.latitude + "," + data.longitude + "?exclude=minutely&exclude=hourly&exclude=daily&exclude=flags&exclude=offset&units=si&callback=testInt"
   console.log(link_needed);
   //JSONP method
-  //let url = "https://api.darksky.net/forecast/f77205dbd3303a32b0147c56de128ed0/";
+  //let url = "https://api.darksky.net/forecast/2f325944bde3b00d86bdabaf4ac091be/";
   // let data =
   // {
   //
@@ -23,26 +27,21 @@ function intPortWeather()
   script.src = link_needed; // set link to sources
   document.body.appendChild(script); // to append script element into body.
 }
-//apikey: 2f325944bde3b00d86bdabaf4ac091be
 
-
-function test(data)
+function testInt(data)
 {
   let listHTML = "";
-  let stuff_needed = [data.latitude, data.longitude, data.currently.summary, data.currently.temperature, data.currently.pressure,data.currently.humidity, data.currently.dewPoint, data.currently.uvIndex];
-  let header_needed = ["Latitude Obtained", "Longitude Obtained","Summary", "Current Temperature (&#8451;) ", "Pressure (Pa)", "Humidity", "Dew Point (&#8451;)","UV Index"];
+  let stuff_needed = [data.currently.summary, data.currently.temperature, data.currently.pressure,data.currently.humidity, data.currently.dewPoint, data.currently.uvIndex];
+  let header_needed = ["Weather", "Current Temperature (&#8451;) ", "Pressure (Pa)", "Humidity", "Dew Point (&#8451;)","UV Index"];
   let symbol_needed = ["&#8451;", "Pa"]
 
   // need to redo .
   for(let i = 0; i < header_needed.length; i++)
   {
     listHTML += "<tr><th>" + header_needed[i] + "</th><td>" + stuff_needed[i] + "</td></tr>" + "<br>";
-    document.getElementById('table-weather').innerHTML = listHTML
+    document.getElementById('intPortWeatherTable').innerHTML = listHTML
   }
 }
-
-
-
     //need to use forloop somehow, problem is between JSONP request and the publish of info. No idea how to extract it.
 
 
@@ -56,3 +55,42 @@ function test(data)
 //    rowHTML+='<tr><th>'+'property name+'</th><td>'+property value+'</td></tr>';
 //
 //<!-- sample rows-->
+
+
+//Weather for final port
+function finPortWeather()
+{
+  document.getElementById("finPortName").innerHTML = JSON.parse(localStorage.getItem('finPort')).portName;
+  let data =
+    {
+      latitude: JSON.parse(localStorage.getItem('finPort')).lat,
+      longitude: JSON.parse(localStorage.getItem('finPort')).lng
+    };
+  let data_needed = {
+    exclude:["minutely","hourly","daily","flags","offset"],
+    units:"si"
+  };
+  let link_needed = "https://api.darksky.net/forecast/2f325944bde3b00d86bdabaf4ac091be/" + data.latitude + "," + data.longitude + "?exclude=minutely&exclude=hourly&exclude=daily&exclude=flags&exclude=offset&units=si&callback=testFin"
+  console.log(link_needed);
+  let script = document.createElement('script'); // create script element in HTML
+  script.src = link_needed; // set link to sources
+  document.body.appendChild(script); // to append script element into body.
+}
+function testFin(data)
+{
+  let listHTML = "";
+  let stuff_needed = [data.currently.summary, data.currently.temperature, data.currently.pressure,data.currently.humidity, data.currently.dewPoint, data.currently.uvIndex];
+  let header_needed = ["Weather", "Current Temperature (&#8451;) ", "Pressure (Pa)", "Humidity", "Dew Point (&#8451;)","UV Index"];
+  let symbol_needed = ["&#8451;", "Pa"]
+
+  for(let i = 0; i < header_needed.length; i++)
+  {
+    listHTML += "<tr><th>" + header_needed[i] + "</th><td>" + stuff_needed[i] + "</td></tr>" + "<br>";
+    document.getElementById('finPortWeatherTable').innerHTML = listHTML
+  }
+}
+
+
+
+
+document.getElementById("fuelOfTheBoat").innerHTML = JSON.parse(localStorage.getItem('myShip')).range;
