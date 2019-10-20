754b06aa187497c1c2c4e8253f8d77234b7dd71d
yizhetang <55679945+yizhetang@users.noreply.github.com>
2019-10-20 18:45:16 +0800

    Update weather.js, add finport weather

--- Weather.js
+++ Weather.js
@@ -1,6 +1,5 @@
 //Weather of initial port
 //var ts = Math.round((new Date(2019,12,2,9,0)).getTime() / 1000); change to UNIX
-let time;
 let dateSet = JSON.parse(localStorage.getItem('date'));
 let year = dateSet.substring(0,4);
 //month have to minus 1 to have the correct data
@@ -21,8 +20,7 @@ for (let i = 0; i < 7; i++)
 }
 localStorage.setItem('unixTimeSevenDays', JSON.stringify(unixTimeSevenDays))
 unixTimeSevenDays.forEach(element => intPortWeather(element));
-let store = [];
-let num = 0;
+let time;
 
 function intPortWeather(time)
 {
@@ -52,6 +50,9 @@ function intPortWeather(time)
   script.src = link_needed; // set link to sources
   document.body.appendChild(script); // to append script element into body.
 }
+
+let store = [];
+let num = 0;
 
 function testInt(data)
 {
@@ -91,20 +92,7 @@ for (let c = 0; c < JSON.parse(localStorage.getItem('store')).length; c++)
 }
 document.getElementById('intPortWeatherTable').innerHTML += rowsA;
 
-let rowsB = "<tr><th>Date</th><th>Weather</th><th>Current temperature (℃) </th></tr>" ;
-let dateB = parseInt(JSON.parse(localStorage.getItem('date')).split('-')[2]);
-let monthB = parseInt(JSON.parse(localStorage.getItem('date')).split('-')[1]);
-for (let c = 0; c < JSON.parse(localStorage.getItem('store')).length; c++)
-{
-    rowsB += "<tr>";
 
-    rowsB += "<td>" + dateB + '/' + monthB + "</td>";
-    rowsB += "<td>" + JSON.parse(localStorage.getItem('store'))[c][0] + "</td>";
-    rowsB += "<td>" + JSON.parse(localStorage.getItem('store'))[c][1] + "</td>"
-    rowsB += "</tr>";
-    dateB++;
-}
-document.getElementById('finPortWeatherTable').innerHTML += rowsB;
 
 
 
@@ -123,9 +111,9 @@ document.getElementById('finPortWeatherTable').innerHTML += rowsB;
 //
 //<!-- sample rows-->
 
-
+unixTimeSevenDays.forEach(element => finPortWeather(element));
 //Weather for final port
-function finPortWeather()
+function finPortWeather(time)
 {
   document.getElementById("finPortName").innerHTML = JSON.parse(localStorage.getItem('portCoord')).name[1];
   let data =
@@ -137,31 +125,48 @@ function finPortWeather()
     exclude:["minutely","hourly","daily","flags","offset"],
     units:"si"
   };
-  finCurrentTime = Number(JSON.parse(localStorage.getItem('unixTime'))) + 10000;
-  let link_needed = "https://api.darksky.net/forecast/2f325944bde3b00d86bdabaf4ac091be/" + data.latitude + "," + data.longitude + "," + finCurrentTime +"?exclude=minutely&exclude=hourly&exclude=daily&exclude=flags&exclude=offset&units=si&callback=testFin"
+  let link_needed = "https://api.darksky.net/forecast/2f325944bde3b00d86bdabaf4ac091be/" + data.latitude + "," + data.longitude + "," + `${time}` +"?exclude=minutely&exclude=hourly&exclude=daily&exclude=flags&exclude=offset&units=si&callback=testFin"
   console.log(link_needed);
   let script = document.createElement('script'); // create script element in HTML
   script.src = link_needed; // set link to sources
   document.body.appendChild(script); // to append script element into body.
 }
 
-
+let storeFin = [];
+let numFin = 0;
 function testFin(data)
 {
   let listHTML = "";
   let stuff_needed = [JSON.parse(localStorage.getItem('day')) + "/" + (Number(JSON.parse(localStorage.getItem('month'))) + 1), data.currently.summary, data.currently.temperature];
   let header_needed = ["Time", "Weather", "Current Temperature (&#8451;) "];
   let symbol_needed = ["&#8451;"]
-  for(let i = 0; i < header_needed.length; i++)
-  {
-    listHTML += "<tr><th>" + header_needed[i] + "</th><td>" + stuff_needed[i] + "</td></tr>" + "<br>";
-    document.getElementById('finPortWeatherTable').innerHTML = listHTML;
-  }
-  let weatherFinOverall = '.The ship will arrive at the destination port after 1 day. Therefore, the destination port date will be the 2019-10-18 and the weather forecast will be ' + data.currently.summary + ".";
-  document.getElementById('weatherFinSummary').innerHTML = weatherFinOverall;
-  localStorage.setItem('finWeather',JSON.stringify(data.currently.summary));
+
+  storeFin[numFin] = [data.currently.summary, data.currently.temperature]
+  numFin += 1;
+  localStorage.setItem('storeFin', JSON.stringify(storeFin))
+//  let weatherFinOverall = '.The ship will arrive at the destination port after 1 day. Therefore, the destination port date will be the 2019-10-18 and the weather forecast will be ' + data.currently.summary + ".";
+//  document.getElementById('weatherFinSummary').innerHTML = weatherFinOverall;
+//  localStorage.setItem('finWeather',JSON.stringify(data.currently.summary));
 }
 
+let rowsB = "<tr><th>Date</th><th>Weather</th><th>Current temperature (℃) </th></tr>" ;
+let dateB = parseInt(JSON.parse(localStorage.getItem('date')).split('-')[2]);
+let monthB = parseInt(JSON.parse(localStorage.getItem('date')).split('-')[1]);
+for (let c = 0; c < JSON.parse(localStorage.getItem('storeFin')).length; c++)
+{
+    rowsB += "<tr>";
+
+    rowsB += "<td>" + dateB + '/' + monthB + "</td>";
+    rowsB += "<td>" + JSON.parse(localStorage.getItem('storeFin'))[c][0] + "</td>";
+    rowsB += "<td>" + JSON.parse(localStorage.getItem('storeFin'))[c][1] + "</td>"
+    rowsB += "</tr>";
+    dateB++;
+}
+
+function 
+document.getElementById('finPortWeatherTable').innerHTML += rowsB;
+
+//print out results
 document.getElementById("totalDistance").innerHTML = JSON.parse(localStorage.getItem('routeDistance'))/1000;
 document.getElementById("fuelOfTheBoat").innerHTML = JSON.parse(localStorage.getItem('myShip')).range;
 document.getElementById("totalCost").innerHTML = JSON.parse(localStorage.getItem('myShip')).cost*document.getElementById("totalDistance").value;
